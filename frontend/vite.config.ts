33d7faf37e0add63be53c2ee415b88c529ffde84
miladgiotin074 <miladgiotin074@gmail.com>
2025-02-15 17:53:56 -0800

    good2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: ['es2022', 'chrome91', 'edge91', 'firefox89', 'safari15']
-  }
-
+    target: 'esnext',
+  },
 })
